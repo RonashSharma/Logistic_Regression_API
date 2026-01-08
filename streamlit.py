0f2d4cfecee40672e5b3d0f97bde343eeb5d0d7e
RonashSharma <sharmaronash@icloud.com>
2026-01-08 22:03:47 +0545

    streamlit added

---
+++ streamlit.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import requests
+import pandas as pd 
+
+API_URL="https://logistic-regression-api-1-vtx3.onrender.com/predict"
+
+emp_Satisfaction = st.slider(
+    'Employee Satisfaction',
+    min_value=1,
+    max_value=5,
+    value=3,
+    step=1
+)
+
+if st.button("Predict Termination"):
+    payload={
+        "EmpSatisfaction":emp_Satisfaction
+    }
+
+    try:
+        response= requests.post(API_URL,json=payload)
+
+        if response.status_code == 200:
+            result = response.json()
+
+            if result["Predicted_Termination"] == 1:
+                st.error("Employee is likely to be terminated!")
+            else:
+                st.success("Employee is likely to remain active!")
+        else:
+            st.warning("API ERROR !!")
+    except requests.expectaions.RequestException:
+        st.error("Could not connect")
+
