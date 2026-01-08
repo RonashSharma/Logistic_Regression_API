dfd1fbe27959270b7e3a14944b58b311d435f942
RonashSharma <sharmaronash@icloud.com>
2026-01-08 14:35:02 +0545

    first commit

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from app.model import load_model, features
+import pandas as pd
+from app.schema import EmployeeTerm
+from fastapi import FastAPI
+
+app = FastAPI()
+
+model = load_model()
+
+#API EndPoints
+@app.get("/")
+def home():
+    return "Home Page"
+@app.post("/predict")
+def predict(data:EmployeeTerm):
+    input_data =pd.DataFrame([
+        data.dict()
+    ],columns=features)
+
+    prediction = model.predict(input_data)[0]
+    prob = model.predict_proba(input_data)[0]
+
+    return{
+        "Prediction_Termination":int(prediction),
+        "Status":"Termination" if prediction == 1 else "Active",
+        "Probability":{
+            "Active":prob[0],
+            "Terminated":prob[1]
+        }
+    }
---
+++ train_model.py
@@ -0,0 +1,6 @@
+from app.model import train_model
+
+if __name__ == "__main__":
+    train_model()
+    print("Model trained and saved successfully!!")
+
